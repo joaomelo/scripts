500ef501853e9b60422c5d3fd401ba81b9d61203
joão melo <j@melo.plus>
2022-08-24 14:44:56 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-# should be saved inside the poweshell subfolder on home directory
+# should be saved inside the poweshell subfolder on home directory or onedrive "..\OneDrive\Documentos\PowerShell"
 oh-my-posh init pwsh config ~/jm.omp.json | Invoke-Expression
 Import-Module -Name Terminal-Icons
